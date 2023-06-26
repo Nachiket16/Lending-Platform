158223dd5264e25414d6d3103ffcc17178e046c4
Nachiket16 <nachiketkhule.nk@gmail.com>
2023-06-26 19:23:24 +0530

    refactor

--- company-api/src/main/java/com/mintifi/companyapi/exception/ResourceNotFoundException.java
+++ company-api/src/main/java/com/mintifi/companyapi/exception/ResourceNotFoundException.java
@@ -5,32 +5,36 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ResourceNotFoundException extends RuntimeException{
-    String resourceName;
-    String fieldName;
-    long fieldValue;
+public class ResourceNotFoundException extends RuntimeException {
 
-    String fieldMessage;
+  String resourceName;
+  String fieldName;
+  long fieldValue;
 
-    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
-        super(String.format("%s not found with %s : %s", resourceName,fieldName,fieldValue));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
-    }
-    public ResourceNotFoundException(String resourceName, String fieldName, String fieldMessage) {
-        super(String.format("%s not found with %s : %s", resourceName,fieldName,fieldMessage));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldMessage = fieldMessage;
-    }
-    public ResourceNotFoundException(String fieldName) {
-        super(String.format("not found with %s ",fieldName));
-        this.fieldName = fieldName;
-    }
-public ResourceNotFoundException(long fieldValue) {
-        super(String.format("not found with id %s ",fieldValue));
-        this.fieldValue = fieldValue;
-    }
+  String fieldMessage;
+
+  public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+    super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue));
+    this.resourceName = resourceName;
+    this.fieldName = fieldName;
+    this.fieldValue = fieldValue;
+  }
+
+  public ResourceNotFoundException(String resourceName, String fieldName, String fieldMessage) {
+    super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldMessage));
+    this.resourceName = resourceName;
+    this.fieldName = fieldName;
+    this.fieldMessage = fieldMessage;
+  }
+
+  public ResourceNotFoundException(String fieldName) {
+    super(String.format("not found with %s ", fieldName));
+    this.fieldName = fieldName;
+  }
+
+  public ResourceNotFoundException(long fieldValue) {
+    super(String.format("not found with id %s ", fieldValue));
+    this.fieldValue = fieldValue;
+  }
 
 }
